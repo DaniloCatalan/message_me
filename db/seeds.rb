9ba5e222559db253911bdc3629944b42b1f23f5d
Danilo P Catalan <danilo.e.catalan@gmail.com>
2021-06-05 16:34:53 -0400

    add user resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Jhon", password:"password")
+User.create(username: "Peter", password:"password")
+User.create(username: "Mike", password:"password")
+User.create(username: "Mel", password:"password")
---
+++ test/models/user_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(username: "danilo",password: "password")
+  end
+
+  test "user shoul be valid" do
+    assert @user.valid?
+  end 
+
+  test "name should be present" do
+    @user.username = " "
+    assert_not @user.valid?
+  end
+
+  test "name should be unique" do
+    @user.save
+    @user2 = User.new(username: "danilo")
+    assert_not @user2.valid? 
+  end
+
+  test "name should not be too short" do
+    @user.username = "aa" 
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.username = "a"*16 
+    assert_not @user.valid?
+  end
+
+end
